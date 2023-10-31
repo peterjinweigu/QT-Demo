dd53f4bd835ae5a80f959d21284563d016fc0462
Edwin Zheng <20777515+Lucien950@users.noreply.github.com>
2023-10-31 10:32:37 -0700

    formatting

--- ui/mainwindow/mainwindow.cpp
+++ ui/mainwindow/mainwindow.cpp
@@ -5,24 +5,23 @@
 using std::cout, std::endl;
 
 void MainWindow::CanTX() {
-    cout << "Hello World!" << endl;
+	cout << "Hello World!" << endl;
 }
 
 void MainWindow::CanRX() {
-    cout << "Hello World!" << endl;
+	cout << "Hello World!" << endl;
 }
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
-    connect(canTXTimer, &QTimer::timeout, this, &MainWindow::CanTX);
-    connect(canRXTimer, &QTimer::timeout, this, &MainWindow::CanRX);
-    canTXTimer->start(10);
-    canRXTimer->start(10);
+	connect(canTXTimer, &QTimer::timeout, this, &MainWindow::CanTX);
+	connect(canRXTimer, &QTimer::timeout, this, &MainWindow::CanRX);
+	canTXTimer->start(10);
+	canRXTimer->start(10);
 
-    ui->setupUi(this);
+	ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+	delete ui;
 }
 
--- ui/mainwindow/mainwindow.h
+++ ui/mainwindow/mainwindow.h
@@ -8,18 +8,21 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow() override;
-    static void CanRX();
-    static void CanTX();
+	explicit MainWindow(QWidget *parent = nullptr);
+
+	~MainWindow() override;
+
+	static void CanRX();
+
+	static void CanTX();
 
 private:
-    Ui::MainWindow *ui;
-    QTimer *canRXTimer = new QTimer(this), *canTXTimer = new QTimer(this);
+	Ui::MainWindow *ui;
+	QTimer *canRXTimer = new QTimer(this), *canTXTimer = new QTimer(this);
 };
+
 #endif // MAINWINDOW_H
